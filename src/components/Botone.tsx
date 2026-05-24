ed9efcb8749e21baaf17b6a19af4bfd1c23a5ed9
Alex <al618ex@gmail.com>
2026-05-24 17:48:59 -0500

    hidden

--- src/components/Botone.tsx
+++ src/components/Botone.tsx
@@ -16,9 +16,11 @@ const Botone = ({ children }: BotProps) => {
     <button
       type="button"
       onClick={handleClick}
-      className={color ? "bg-green-500" : "bg-blue-500"}
+      className={`px-4 py-2 min-w-32 text-white ${
+        color ? "bg-green-500" : "bg-blue-500"
+      }`}
     >
-      {hidden ? null : children}
+      {hidden ? "\u00A0" : children}
     </button>
   );
 };
